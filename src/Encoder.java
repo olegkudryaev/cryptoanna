9c8bdf53bf87336aacb993f328526756b246012e
olegkudryaev <oleg.kudraev@gmail.com>
2022-08-08 19:04:47 +0300

    1 и 2 готово

---
+++ src/Encoder.java
@@ -0,0 +1,99 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
+public class Encoder {
+    static int key = 0;
+    static String russianAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    static String englishAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String symbols = "\r\n.?!,;: -()\"";
+    static String numbers = "1234567890";
+    static String CryptoAlphabet = englishAlphabet + russianAlphabet + symbols + numbers;
+    static char[] chars = CryptoAlphabet.toCharArray();
+
+//    static String CryptoAlphabet = "ja";
+//    static char[] chars = CryptoAlphabet.toCharArray();
+
+    //Читаем текст и сохраняем его в СтрингБилдере
+
+    StringBuilder readFile(Path path) throws IOException {
+        List<String> list = Files.readAllLines(path);
+        StringBuilder text = new StringBuilder();
+        for (String str : list) {
+            text.append(str);
+        }
+        System.out.println(text);
+        return text;
+    }
+
+    //Берем стринг билдер и сдвигаем символы по альфавиту на ключ
+    StringBuilder moveText(StringBuilder text, int number) {
+        StringBuilder newText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            for (int j = 0; j < chars.length; j++) {
+                if (symbol == chars[j]) {
+                    //                   System.out.print("Исходный символ " + symbol + " ");
+                    int indexChar = j;
+                    if (indexChar + number >= chars.length) {
+                        symbol = chars[(j - chars.length) + number];
+                        newText.append(symbol);
+                        System.out.println(newText.toString());
+                        break;
+                    }
+                    symbol = chars[j + 2];
+                    //                  System.out.println("и измененный символ " + symbol);
+                    newText.append(symbol);
+                    break;
+                }
+            }
+        }
+//        System.out.println(newText);
+        return newText;
+    }
+
+    //Сдвинутый билдер грузим в файл
+    void textCryptoLoading(StringBuilder newText, Path path) throws IOException {
+        Files.createFile(path);
+        Files.writeString(path, newText);
+    }
+
+    StringBuilder readCryptoFile() throws IOException {
+        Path path = Path.of("F:\\Test\\Test2.txt");
+        List<String> list = Files.readAllLines(path);
+        StringBuilder text = new StringBuilder();
+        for (String str : list) {
+            text.append(str);
+        }
+        return text;
+    }
+
+    StringBuilder moveCryptoText(StringBuilder text) {
+        StringBuilder newText = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = text.charAt(i);
+            for (int j = 0; j < chars.length; j++) {
+                if (symbol == chars[j]) {
+//                   System.out.print("Исходный символ " + symbol + " ");
+                    symbol = chars[j - 1];
+//                   System.out.println("и измененный символ " + symbol);
+                    newText.append(symbol);
+                    break;
+                }
+            }
+        }
+        System.out.println(newText);
+        return newText;
+    }
+
+    StringBuilder textNormalLoading(StringBuilder newText) throws IOException {
+        Files.createFile(Path.of("F:\\Test\\Text3.txt"));
+        String str = newText.toString();
+        Files.writeString(Path.of("F:\\Test\\Text3.txt"), str);
+        return null;
+    }
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+        Encoder encoder = new Encoder();
+        Scanner consoleText = new Scanner(System.in);
+        System.out.println("Выбери действие, введя его номер.\n" +
+                "1 Зашифровать текст в файле с помощью ключа\n" +
+                "2 Расшифровать текст в файле с помощью ключа \n" +
+                "3  Криптоанализ текса в файле (выбрать метод)\n" +
+                "A метод Brute force\n" +
+                "B метод статистического анализа \n" +
+                "Для выхода из программы введи exit");
+        System.out.println();
+        System.out.println("Выберите метод");
+        while (consoleText.hasNextLine()) {
+            String question = consoleText.nextLine();
+            if (question.equals("1")) {
+//                fileAddressRequestforEncryption();
+
+                System.out.println("Введите путь до файла");
+                Path pathToFile = Path.of(consoleText.nextLine());
+                StringBuilder text = encoder.readFile(pathToFile);
+
+                System.out.println("Введите ключ от 1 до 100");
+                int number = Integer.parseInt(consoleText.nextLine());
+                StringBuilder moveText = encoder.moveText(text, number);
+
+                System.out.println("Введите адрес, для создания файла с зашифрованным текстом");
+                String str = consoleText.nextLine();
+                Path pathToCrFile = Path.of(str);
+                encoder.textCryptoLoading(moveText, pathToCrFile);
+
+                break;
+            } else if (question.equals("2")) {
+//                fileAddressRequestforDecryption();
+                break;
+            } else if (question.equals("3")) {
+                System.out.println(" Вы выбрали криптоанализ\n" +
+                        "Выберите какой метод будете использовать?\n" +
+                        "A метод Brute force\n" +
+                        "B метод статистического анализа \n" +
+                        "Для выхода из программы введи exit");
+                String choiceOfMethod = consoleText.nextLine();
+                if (choiceOfMethod.equals("A")) {
+                    System.out.println("Выбран метод Brute force");
+                    //           fileAddressRequestforBruteForce();
+                    break;
+                } else if (choiceOfMethod.equals("B")) {
+                    System.out.println("Выбран метод статистического анализа");
+//                    fileAddressRequestforStats();
+                    break;
+                } else {
+                    System.out.println("Ввели недопустимые значения");
+                }
+            } else if (question.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Ввели недопустимые значения");
+            }
+        }
+        System.out.println("Ввели недопустимые значения");
+
+//        Encoder encoder = new Encoder();
+//       StringBuilder str = new StringBuilder("a");
+//                StringBuilder normalText = encoder.readFile();
+//                StringBuilder answerText = encoder.moveText(normalText);
+//        encoder.textCryptoLoading(answerText);
+//       StringBuilder cryptoText = encoder.readCryptoFile();
+//       StringBuilder answerText2 = encoder.moveCryptoText(cryptoText);
+//      encoder.textCryptoLoading(answerText2);
+
+    }
+}
